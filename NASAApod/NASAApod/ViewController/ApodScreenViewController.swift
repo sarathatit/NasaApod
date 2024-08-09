78d5b3b1bed765bd6e998b5e41da27972c3a0bbe
Sarath <53928677+sarathatit@users.noreply.github.com>
2024-08-09 15:04:35 +0530

    saving data in local and implemented network Monitering

--- NASAApod/NASAApod/ViewController/ApodScreenViewController.swift
+++ NASAApod/NASAApod/ViewController/ApodScreenViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import Combine
 
 class ApodScreenViewController: UIViewController {
 
+    // MARK: - Outlets Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var viewModel: ApodScreenViewModel!
+    
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewModel = ApodScreenViewModel(service: WebService())
+        observeViewModel()
     }
-
+    
+    // MARK: - Observer
+    func observeViewModel() {
+        viewModel.$apodModel
+            .sink { [weak self] model in
+                self?.updateUI(with: model)
+            }
+            .store(in: &viewModel.cancellable)
+    }
+    
+    // MARK: - UI Update
+    func updateUI(with model: ApodModel?) {
+        
+    }
 
 }
 
--- NASAApod/NASAApod/ViewModel/ApodScreenViewModel.swift
+++ NASAApod/NASAApod/ViewModel/ApodScreenViewModel.swift
@@ -6,7 +6,84 @@
 //
 
 import Foundation
+import Combine
+import Network
 
-class ApodScreenViewModel {
+class ApodScreenViewModel: ObservableObject {
     
+    @Published var apodModel: ApodModel?
+    var cancellable = Set<AnyCancellable>()
+    let service: WebService
+    var monitor: NWPathMonitor?
+    var isConnected: Bool = true
+    
+    init(service: WebService) {
+        self.service = service
+        startNetworkMonitoring()
+        getData()
+    }
+    
+    deinit {
+        stopNetworkMonitoring()
+    }
+    
+    // MARK: - Service Call Methods
+    private func getData() {
+        if isConnected {
+            service.getData()
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("error", error)
+                    case .finished:
+                        print("completion")
+                    }
+                } receiveValue: { [weak self] respones in
+                    self?.apodModel = respones
+                    self?.saveData(respones)
+                }
+                .store(in: &self.cancellable)
+        } else {
+            loadStoredData()
+        }
+    }
+
+    // MARK: - Local Saving
+    private func saveData(_ apod: ApodModel) {
+        if let encoded = try? JSONEncoder().encode(apod) {
+            UserDefaults.standard.set(encoded, forKey: "cachedApod")
+            UserDefaults.standard.set(Date(), forKey: "lastSeenDate")
+        }
+    }
+    
+    private func loadStoredData() {
+        if let savedApodData = UserDefaults.standard.data(forKey: "cachedApod"),
+           let lastSeenDate = UserDefaults.standard.object(forKey: "lastSeenDate") as? Date{
+            apodModel = try? JSONDecoder().decode(ApodModel.self, from: savedApodData)
+        }
+    }
+}
+
+// MARK: - NetWork methods
+
+extension ApodScreenViewModel {
+    
+        private func startNetworkMonitoring() {
+            monitor = NWPathMonitor()
+            let queue = DispatchQueue.global(qos: .background)
+            monitor?.start(queue: queue)
+            
+            monitor?.pathUpdateHandler = { [weak self] path in
+                self?.isConnected = path.status == .satisfied
+                if self?.isConnected == false {
+                    DispatchQueue.main.async {
+                        self?.loadStoredData()
+                    }
+                }
+            }
+        }
+        
+        private func stopNetworkMonitoring() {
+            monitor?.cancel()
+        }
 }
